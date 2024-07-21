0dc7eaf730c9ef260697b2bb0ad5d3974dbf9753
Muthukumar1104 <ksrmk437@gmail.com>
2024-07-22 01:07:15 +0530

    Netlify server deployemnt changes

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,29 @@
 const express = require("express");
-const serverless = require("serverless-http");
-const app = express();
+const cors = require("cors");
+const connectDB = require("./config/db-config");
 require("dotenv").config();
+
+connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
-app.use("/.netlify/functions/api", require("../routes/index-route"));
+app.get("/", (req, res) => {
+  res.send("Success!");
+});
+
+app.use("/.netlify/functions/api", require("./routes/index-route"));
+
+const Port = process.env.port || 4000;
+app.listen(Port, () => {
+  console.log(`Application running successfully on ${Port}`);
+});
 
 module.exports.handler = serverless(app);
